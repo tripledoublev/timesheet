429bf25fde15b7bdfb845c8cbf80eeb8887d2bc4
tripledoublev <vincentcharlebois@gmail.com>
2022-05-22 16:30:11 -0400

    Added weather sensing module

---
+++ climate.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python3
+
+import time
+import datetime
+import colorsys
+import sys
+import ST7735
+try:
+    # Transitional fix for breaking change in LTR559
+    from ltr559 import LTR559
+    ltr559 = LTR559()
+except ImportError:
+    import ltr559
+
+from bme280 import BME280
+from enviroplus import gas
+from subprocess import PIPE, Popen
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from fonts.ttf import RobotoMedium as UserFont
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.info("""combined.py - Displays readings from all of Enviro plus' sensors
+
+Press Ctrl+C to exit!
+
+""")
+
+# BME280 temperature/pressure/humidity sensor
+bme280 = BME280()
+
+
+# Create ST7735 LCD display class
+st7735 = ST7735.ST7735(
+    port=0,
+    cs=1,
+    dc=9,
+    backlight=12,
+    rotation=270,
+    spi_speed_hz=10000000
+)
+
+# Initialize display
+st7735.begin()
+
+WIDTH = st7735.width
+HEIGHT = st7735.height
+
+# Set up canvas and font
+img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+draw = ImageDraw.Draw(img)
+font_size_small = 12
+font_size_large = 18
+font = ImageFont.truetype("FreeSans.ttf", font_size_large)
+smallfont = ImageFont.truetype("FreeSans.ttf", font_size_small)
+x_offset = 4
+y_offset = 4
+
+message = ""
+
+# The position of the top bar
+top_pos = 25
+timeNow = round(time.time())
+# Create a values dict to store the data
+variables = ["temperature",
+             "pressure",
+             "humidity",
+             "light"
+             ]
+
+units = ["C",
+         "hPa",
+         "%",
+         "Lux"
+         ]
+
+# Define your own warning limits
+# The limits definition follows the order of the variables array
+# Example limits explanation for temperature:
+# [4,18,28,35] means
+# [-273.15 .. 4] -> Dangerously Low
+# (4 .. 18]      -> Low
+# (18 .. 28]     -> Normal
+# (28 .. 35]     -> High
+# (35 .. MAX]    -> Dangerously High
+# DISCLAIMER: The limits provided here are just examples and come
+# with NO WARRANTY. The authors of this example code claim
+# NO RESPONSIBILITY if reliance on the following values or this
+# code in general leads to ANY DAMAGES or DEATH.
+limits = [[4, 18, 28, 35],
+          [250, 650, 1013.25, 1015],
+          [20, 30, 60, 70],
+          [-1, -1, 30000, 100000]]
+
+
+# RGB palette for values on the combined screen
+palette = [(0, 0, 255),           # Dangerously Low
+           (0, 255, 255),         # Low
+           (0, 255, 0),           # Normal
+           (255, 255, 0),         # High
+           (255, 0, 0)]           # Dangerously High
+
+values = {}
+
+
+# Displays data and text on the 0.96" LCD
+def display_text(variable, data, unit):
+    # Maintain length of list
+    values[variable] = values[variable][1:] + [data]
+    # Scale the values for the variable between 0 and 1
+    vmin = min(values[variable])
+    vmax = max(values[variable])
+    colours = [(v - vmin + 1) / (vmax - vmin + 1) for v in values[variable]]
+    # Format the variable name and value
+    message = "{}: {:.1f} {}".format(variable, data, unit)
+    logging.info(message)
+    draw.rectangle((0, 0, WIDTH, HEIGHT), (255, 255, 255))
+    for i in range(len(colours)):
+        # Convert the values to colours from red to blue
+        colour = (1.0 - colours[i]) * 0.6
+        r, g, b = [int(x * 255.0) for x in colorsys.hsv_to_rgb(colour, 1.0, 1.0)]
+        # Draw a 1-pixel wide rectangle of colour
+        draw.rectangle((i, top_pos, i + 1, HEIGHT), (r, g, b))
+        # Draw a line graph in black
+        line_y = HEIGHT - (top_pos + (colours[i] * (HEIGHT - top_pos))) + top_pos
+        draw.rectangle((i, line_y, i + 1, line_y + 1), (0, 0, 0))
+    # Write the text at the top in black
+    draw.text((0, 0), message, font=font, fill=(0, 0, 0))
+    st7735.display(img)
+
+
+# Saves the data to be used in the graphs later and prints to the log
+def save_data(idx, data):
+    variable = variables[idx]
+    # Maintain length of list
+    values[variable] = values[variable][1:] + [data]
+    unit = units[idx]
+    message = "{}: {:.1f} {}".format(variable[:4], data, unit)
+    logging.info(message)
+
+
+# Displays all the text on the 0.96" LCD
+def display_everything():
+    draw.rectangle((0, 0, WIDTH, HEIGHT), (0, 0, 0))
+    column_count = 1
+    row_count = (len(variables) / column_count)
+    for i in range(len(variables)):
+        variable = variables[i]
+        data_value = values[variable][-1]
+        unit = units[i]
+        x = x_offset + ((WIDTH // column_count) * (i // row_count))
+        y = y_offset + ((HEIGHT / row_count) * (i % row_count))
+        message = "{}: {:.1f} {}".format(variable, data_value, unit)
+        lim = limits[i]
+        rgb = palette[0]
+        for j in range(len(lim)):
+            if data_value > lim[j]:
+                rgb = palette[j + 1]
+        draw.text((x, y), message, font=smallfont, fill=rgb)
+    st7735.display(img)
+
+def display_seconds():
+    # Width and height to calculate text position
+    WIDTH = st7735.width
+    HEIGHT = st7735.height
+    ts = int(time.time() - 1652025600)
+    # Text settings
+    font_size = 24
+    font = ImageFont.truetype(UserFont, font_size)
+    text_colour = (255, 255, 255)
+    back_colour = (0, 0, 0) 
+    message = str(ts)
+    img = Image.new("RGB", (WIDTH, HEIGHT), color=(0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    size_x, size_y = draw.textsize(message, font)
+    x = (WIDTH - size_x) / 2
+    y = (HEIGHT / 2) - (size_y / 2)
+    draw.rectangle((0, 0, 160, 80), back_colour)
+    draw.text((x, y), message, font=font, fill=text_colour)
+    st7735.display(img)
+# Get the temperature of the CPU for compensation
+def get_cpu_temperature():
+    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
+    output, _error = process.communicate()
+    return float(output[output.index('=') + 1:output.rindex("'")])
+
+
+def main():
+    count = 0
+    # Tuning factor for compensation. Decrease this number to adjust the
+    # temperature down, and increase to adjust up
+    factor = 1.25
+
+    cpu_temps = [get_cpu_temperature()] * 5
+
+    delay = 0.5  # Debounce the proximity tap
+    mode = 5   # The starting mode
+    last_page = 0
+
+    for v in variables:
+        values[v] = [1] * WIDTH
+
+    # The main loop
+    try:
+        while True:
+            # Everything on one screen
+            cpu_temp = get_cpu_temperature()
+            # Smooth out with some averaging to decrease jitter
+            cpu_temps = cpu_temps[1:] + [cpu_temp]
+            avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
+            raw_temp = bme280.get_temperature()
+            raw_data = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
+            save_data(0, raw_data)
+            temp = raw_data
+            display_everything()
+            raw_data = bme280.get_pressure()
+            save_data(1, raw_data)
+            press = raw_data
+            display_everything()
+            raw_data = bme280.get_humidity()
+            save_data(2, raw_data)
+            humi = raw_data
+            display_everything()
+            raw_data = ltr559.get_lux()
+            save_data(3, raw_data)
+            lux = raw_data
+            display_everything()
+            timeNow = round(time.time())
+            degree_sign = u"\N{DEGREE SIGN}"
+            count += 1
+            time.sleep(3)
+            if count % 100 == 1: 
+               with open("data.txt", "a+") as data_archive:
+                  timeNow = round(time.time())
+                  dateNow = datetime.datetime.fromtimestamp(timeNow)
+                  data_archive.seek(0)
+                  data_archive.write("\n")
+                  data_archive.write(str(timeNow) + " " + str(round(temp, 2)) + str(degree_sign) + "C " + str(round(humi, 2)) + "% " + str(round(lux, 2)) + "Lux " + str(round(press,2)) + "hPa " + str(dateNow))
+                  print(str(dateNow) + " " + str(timeNow) + " " +str(round(temp, 2)) + str(degree_sign) + "C" + " " + str(round(humi, 2)) + "%" + " " + str(round(lux, 2)) + "Lux" + " " + str(round(press,2)) + "hPa")
+
+            
+    # Exit cleanly
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
