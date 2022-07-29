68d302172842cc75fe5aa93585b29262b6f615e6
Vincent Charlebois <vincentcharlebois@gmail.com>
2022-07-29 19:01:01 -0400

    pleeeease

--- modules/weather/archive.py
+++ modules/weather/archive.py
@@ -2,5 +2,9 @@
 # takes temp, humi and lux + timestamp 
 # and appends the data to the archive
 def main(temp, humi, lux, press, time, x):
-    with open("../../data/climate.txt", "a") as a:
+    current = os.path.dirname(__file__)
+    parent = os.path.split(current)[0]
+    another_parent = os.path.split(parent)[0]
+    file_path = os.path.join(another_parent, 'data', 'climate.txt')
+    with open("file_path", "a") as a:
         a.write(x + "Temp: " + temp + " Humi: " + humi + " Lux: " + lux + " Pressure: " + press + time)
