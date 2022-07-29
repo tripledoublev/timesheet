d302e6233c097f1f44785aae1dcb522d6f8174cc
Vincent Charlebois <vincentcharlebois@gmail.com>
2022-07-29 19:33:56 -0400

    cleanup

--- modules/weather/archive.py
+++
@@ -1,8 +0,0 @@
-# runs when --climate is used
-# takes temp, humi and lux + timestamp 
-# and appends the data to the archive
-def main(temp, humi, lux, press, time, x):
-    with open("climate.txt", "a+") as a:
-        a.seek(0) 
-        a.write("\n")
-        a.write(str(x) + "Temp: " + str(temp) + " Humi: " + str(humi) + " Lux: " + str(lux) + " Pressure: " + str(press) + str(time))
