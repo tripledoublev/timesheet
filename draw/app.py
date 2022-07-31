9e47c0dbe7c2ce9d39e6538e30d7598462b357ff
Vincent Charlebois <vincentcharlebois@gmail.com>
2022-07-31 17:08:45 -0400

    ready for the future

---
+++ draw/app.py
@@ -0,0 +1,4 @@
+# This will pull the data of my INs & OUTs of the studio 
+# from consecutive-days/data/and generate the html files
+# that visualize the consecutive days spent at the studio 
+# and the environmental conditions while i was present
