298f818174f15218e2975bdb594b5efcd17e9196
Vincent Charlebois <vincentcharlebois@gmail.com>
2022-07-31 16:42:46 -0400

    cleaner html

--- generate_index.py
+++ generate_index.py
@@ -1,9 +1,25 @@
+# creates index.html from 
+# files in the folder
 import os
 from os import listdir
 from os.path import isfile, join
 onlyfiles = [f for f in listdir('consecutive-days/') if isfile(join('consecutive-days', f))]
 with open('consecutive-days/index.html','w') as f:
-    f.write('<!DOCTYPE html><html><head><title>Consecutive days at the studio</title><link rel="stylesheet" href="css/style.css"></head><body id="dcontain"><h1 id="page-title" class="textColor"><a href="https://github.com/tripledoublev/timesheet/">Consecutive days at the studio</a></h1><div class="flexbox" id="fbox">')
+    f.write('<!DOCTYPE html><html><head><title>Consecutive days at the studio</title>')
+    f.write('<link rel="stylesheet" href="css/style.css"></head>')
+    f.write('<body id="dcontain">')
+    f.write('<h1 id="page-title" class="textColor">')
+    f.write('<a href="https://github.com/tripledoublev/timesheet/">')
+    f.write('Consecutive days at the studio')
+    f.write('</a></h1><div class="flexbox" id="fbox">')
     for file in onlyfiles:
-        f.write('<div class="textColor"><a href="' + file + '">' + file + '</a></div>')
+        # add new line except for first line
+        f.write("\n")
+        if file != 'index.html':
+            fileless = file.split('.')
+            f.write('<div class="textColor">')
+            f.write('<a href="' + file + '">')
+            f.write(fileless[0])
+            f.write('</a>')
+            f.write('</div>')
     f.write('<script type="text/javascript" src="js/script.js"></script></body></html>')
