6280bda7f75e83b92fe77226773d2fa23b3529c1
Vincent Charlebois <vincentcharlebois@gmail.com>
2022-08-07 18:24:08 -0400

    automatically updates the html files

---
+++ compiler.py
@@ -0,0 +1,12 @@
+import timeMachine
+import generate_html
+import generate_index
+from data import export
+
+timeMachine
+export
+generate_html
+generate_index
+print('consecutive days at the studio have been compiled')
+print('update it with commit+push')
+
