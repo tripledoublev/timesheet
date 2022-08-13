2136df0980fae86de74da27477527d237d46dd3f
Vincent Charlebois <vincentcharlebois@gmail.com>
2022-08-13 09:43:57 -0400

    when I leave, compare with temperature when I arrive

--- am-i/js/script.js
+++ am-i/js/script.js
@@ -24,14 +24,14 @@ let myDiv = document.getElementById('textcontainer');
                 let change = temperature[1];
                 if (Math.sign(change) === -1) {
                     otherDiv.innerHTML += ' \u2014 Indoor temperature was ' + past + '\u00B0C when I left';
-                    otherDiv.innerHTML += ' \u2014 ' + (change * -1) + '\u00B0C warmer than last recorded temperature';
-                    otherDiv.innerHTML += ' \u2014 The studio is only one of many places where art gets made.';                    
+                    otherDiv.innerHTML += ' \u2014 ' + (change * -1) + '\u00B0C warmer than when I arrived';
+                    otherDiv.innerHTML += ' \u2014 The studio is only one of many places where art gets made';                    
                     otherDiv.innerHTML += '                                                       ';
                 }
                 else if (Math.sign(change) === 1) {
                     otherDiv.innerHTML += ' \u2014 Indoor temperature was ' + past + '\u00B0C when I left';
-                    otherDiv.innerHTML += ' \u2014 ' + (change * 1) + '\u00B0C cooler than last recorded temperature'
-                    otherDiv.innerHTML += ' \u2014 The studio is only one of many places where art gets made.';                    
+                    otherDiv.innerHTML += ' \u2014 ' + (change * 1) + '\u00B0C cooler than when I arrived'
+                    otherDiv.innerHTML += ' \u2014 The studio is only one of many places where art gets made';                    
                     otherDiv.innerHTML += '                                                       ';
                 }
         });
@@ -55,13 +55,13 @@ let myDiv = document.getElementById('textcontainer');
                 let change = temperature[1];
                 if (Math.sign(change) === -1) {
                     otherDiv.innerHTML += ' \u2014 Indoor temperature was ' + past + '\u00B0C when I left';
-                    otherDiv.innerHTML += ' \u2014 ' + (change * -1) + '\u00B0C warmer than last recorded temperature';
+                    otherDiv.innerHTML += ' \u2014 ' + (change * -1) + '\u00B0C warmer than when I arrived';
                     otherDiv.innerHTML += ' \u2014 The studio is only one of many places where art gets made.';                    
                     otherDiv.innerHTML += '                                                       ';
                 }
                 else if (Math.sign(change) === 1) {
                     otherDiv.innerHTML += ' \u2014 Indoor temperature was ' + past + '\u00B0C when I left';
-                    otherDiv.innerHTML += ' \u2014 ' + (change * 1) + '\u00B0C cooler than last recorded temperature'
+                    otherDiv.innerHTML += ' \u2014 ' + (change * 1) + '\u00B0C cooler than when I arrived'
                     otherDiv.innerHTML += ' \u2014 The studio is only one of many places where art gets made.';                    
                     otherDiv.innerHTML += '                                                       ';
                 }
