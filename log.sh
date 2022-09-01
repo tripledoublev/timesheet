1472a0c8e9e1c4025820e34e338f76fcf92e1352
Vincent <vincentcharlebois@gmail.com>
2022-08-31 21:32:35 -0400

    log.sh to log in and out online, on the blockchain and compile latest data

---
+++ log.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# LOG IN AND OUT OF THE STUDIO
+
+# by using IN or OUT as argument
+# ie. sh compiler.sh IN
+
+# if no arguments are given it will compiled latest data
+# and generate html
+
+# OUT arguments also compiles latest data
+
+
+# for each series of consecutives days
+# spent at the studio,
+# 
+# each rectangular shape appears as the daily
+# total of seconds
+# 
+# This daily total of seconds spent at the studio is used to define
+# its shape, color and its size relative to 
+# the other days in the series
+
+# 1. Compiles timesheet (data/t.txt) into daily seconds (data/s.txt)
+
+# 2. From these daily totals, series of consecutive days' totals
+#    are compiled as separate txt files (consecutive-days/data/...)
+#
+# 3. These text files are used as data for their HTML equivalent
+
+# 4. In the end, the html files are used to populate the index.html
+
+if [ $1 == 'IN' ]
+then 
+    python3 app.py --time IN --climate && cd w3py && python3 toggle.py main
+fi
+if [ $1 == 'OUT' ]
+then
+    python3 app.py --time OUT --climate && cd w3py && python3 toggle.py main && cd .. && python3 timeMachine.py && sudo rm consecutive-days/* || true && sudo rm consecutive-days/data/* || true && python3 data/export.py && python3 generate_html.py && python3 generate_index.py
+else
+    python3 timeMachine.py && sudo rm consecutive-days/* || true && sudo rm consecutive-days/data/* || true && python3 data/export.py && python3 generate_html.py && python3 generate_index.py
+fi
