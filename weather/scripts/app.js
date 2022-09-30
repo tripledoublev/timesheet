db5be70877f664e9bcad3b8936e665495a49ff48
Vincent Charlebois <vincentcharlebois@gmail.com>
2022-09-30 16:50:40 -0400

    draw function

---
+++ weather/scripts/app.js
@@ -0,0 +1,17 @@
+
+function drawLine(ctx, begin, end, stroke = 'black', width = 1) {
+    if (stroke) {
+        ctx.strokeStyle = stroke;
+    }
+
+    if (width) {
+        ctx.lineWidth = width;
+    }
+
+    ctx.beginPath();
+    ctx.moveTo(...begin);
+    ctx.lineTo(...end);
+    ctx.stroke();
+}
+
+
