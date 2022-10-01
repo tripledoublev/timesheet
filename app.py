4f7fd73eafdd4a0e02652c2578107f662f1889da
Vincent Charlebois <vincentcharlebois@gmail.com>
2022-10-01 17:27:45 -0400

    becoming machine

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/modules")
 import climate
 
-#import twitter.post_tweet as tweetMachine
+import twitter.post_tweet as tweetMachine
 
 # parsing arguments
 parser = argparse.ArgumentParser()
@@ -57,7 +57,7 @@ if status is None:
         # if --tweet is present
         if args.tweet:
             # tweet it
-            # tweetMachine.main(textEntry)
+            tweetMachine.main(textEntry)
             print('')
             print('░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░') 
             print('░░░   Your message was sent to twitter.   ░░░')
