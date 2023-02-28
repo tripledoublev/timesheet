feb78b01abc253bc9f85e04b3b82c9efdaa879ce
tripledoublev <vincentcharlebois@gmail.com>
2023-02-28 18:19:37 -0500

    COMPILED: Tue 28 Feb 2023 06:19:27 PM EST

--- final.sh
+++ final.sh
@@ -2,17 +2,26 @@
 
 # final performance
 
-# update environmental data every 2 minutes and tweet it all
+# update environmental data every minute and tweet it all
 # end the year long performance with increased data flow
 
 now=$(date "+%c")
 for I in 1 2 3 4 5 6 7 8 9 10
 do
-    tmp=$(python3 app.py --time IN --climate 2>&1) &&
+
+    now=$(date "+%c") && tmp=$(python3 app.py --time OUT --climate 2>&1) &&
     echo '\t░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'
     echo '\t░░░            Data collected             ░░░'
     echo '\t░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░' && cd w3py && dataout=$(python3 update.py ${tmp} 2>&1) &&
     echo '\t░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'
     echo '\t░░░     Transacted with the blockchain    ░░░'
-    echo '\t░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░' && cd .. && python3 app.py --text "${dataout}" --tweet && sleep 2m
+    echo '\t░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░' && cd .. && python3 app.py --text "${dataout} !_  final chapter of a year long performance. timestamped: ${now} " --tweet && sleep 2m
+    now=$(date "+%c") && tmp=$(python3 app.py --time IN --climate 2>&1) &&
+    echo '\t░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'
+    echo '\t░░░            Data collected             ░░░'
+    echo '\t░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░' && cd w3py && dataout=$(python3 update.py ${tmp} 2>&1) &&
+    echo '\t░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░'
+    echo '\t░░░     Transacted with the blockchain    ░░░'
+    echo '\t░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░' && cd .. && python3 app.py --text "${dataout} !_  final chapter of a year long performance. timestamped: ${now} " --tweet && sleep 2m
+
 done
